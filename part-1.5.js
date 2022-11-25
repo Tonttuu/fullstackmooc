6462240605d747c112181e9825b89ba5a4adf2f9
Tonttuu <70258751+Tonttuu@users.noreply.github.com>
2022-11-25 16:01:05 +0200

    Add files via upload

---
+++ part-1.5.js
@@ -0,0 +1,88 @@
+const Header = (props) => {
+  return (
+    <div>
+      <p>{props.course}</p>
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part} {props.exercises}
+    </p>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part part={props.part1} exercises={props.exercises1} />
+      <Part part={props.part2} exercises={props.exercises2} />
+      <Part part={props.part3} exercises={props.exercises3} />
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <div>
+      <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+    </div>
+  )
+}
+
+/* const App = () => {
+  const course = 'Half Stack application development'
+  const parts = [ 
+  {
+    name: 'Fundamentals of React',
+    exercises: 10
+  },
+  {
+    name: 'Using props to pass data',
+    exercises: 7
+  },
+  {
+    name: 'State of a component',
+    exercises: 14
+  }
+]; */
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+   return (
+    <div>
+      <Header course={course.name} />
+      {course.parts.map((part, index) => {
+        return (
+          <div key={index}>
+            {part.name}
+            {part.exercises}
+          </div>
+        );
+      })}
+      
+    </div>
+     
+  );
+}
+
+export default App
